a8f5b438c1ea46babad648c6df6b67dd09a129c2
Mohit1488 <mohit171488.cse@chitkara.edu.in>
2019-12-09 11:38:47 +0530

    Updates

---
+++ precache-manifest.51de9260db3be907c4711671f02f9636.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34f1f4311c8b0d72789c8943cebc46e2",
+    "url": "/test2/index.html"
+  },
+  {
+    "revision": "0b84e7ac957ee08f814d",
+    "url": "/test2/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bb4720a8740e3ea6cf85",
+    "url": "/test2/static/js/2.1701e13b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test2/static/js/2.1701e13b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0b84e7ac957ee08f814d",
+    "url": "/test2/static/js/main.a6a7dd19.chunk.js"
+  },
+  {
+    "revision": "93810bc3d9f38f75e33b",
+    "url": "/test2/static/js/runtime-main.fcb37dfa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test2/static/media/logo.5d5d9eef.svg"
+  }
+]);
